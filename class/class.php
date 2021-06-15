e3618a6ac231768fee50dfc2c3d34773ffaee9ac
andreicismas <andrei_cismas@yahoo.it>
2021-06-15 09:33:26 +0200

    definito un costruttore

--- class/class.php
+++ class/class.php
@@ -1,10 +1,16 @@
 <?php
 
 class Movie {
+    // variabili d'istanza
     public $title;
     public $year;
     public $genre;
     public $country = "US";
     public $age;
-
+    // definito un costruttore
+    function __construct($movTitle, $movYear, $movGenre) {
+        $this->title = $movTitle;
+        $this->year = $movYear;
+        $this->genre = $movGenre;
+    }
 }
