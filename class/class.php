5e42a2749266c6a4ad522769a5dedf55f2423828
andreicismas <andrei_cismas@yahoo.it>
2021-06-15 09:40:45 +0200

    add public function

--- class/class.php
+++ class/class.php
@@ -5,12 +5,27 @@ class Movie {
     public $title;
     public $year;
     public $genre;
-    public $country = "US";
+    public $country = "Romania";
     public $age;
     // definito un costruttore
     function __construct($movTitle, $movYear, $movGenre) {
         $this->title = $movTitle;
         $this->year = $movYear;
         $this->genre = $movGenre;
+    }
+
+    public function getTitle() {
+        return  $this->title ;
+    }
+
+    public function getMovieAge() {
+        return $this->age = "Released movie - " . $this->year;
+    }
+    
+    public function renderMovie() {
+        echo "<h2  >" . $this->getTitle() . "</h2>";
+        echo "<p> Genere: " . $this->genre . "</p>";
+        echo "<p>Country: " . $this->country . "</p>";
+        echo "<p style = color:red >" . $this->age . "</p>";
     }
 }
