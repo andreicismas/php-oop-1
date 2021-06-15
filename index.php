808f8fe9f67e7ec85c89ba6e28a548317649ecaf
andreicismas <andrei_cismas@yahoo.it>
2021-06-15 12:36:40 +0200

    stampo i film

--- index.php
+++ index.php
@@ -16,4 +16,18 @@ $myMovie_2 = new Movie("A Quiet Place", 2018, " horror film");
 $myMovie_3 = new Movie("Raya and the Last Dragon", 2021, " fantasy");
 $myMovie_4 = new Movie("Sister", 2021, "drama film");
 $myMovie_5 = new Movie("Detective Chinatown 3", 2021 , " comedy-mystery");
-
+//movie 1
+$myMovie_1->getMovieAge();
+$myMovie_1->renderMovie();
+//movie 2
+$myMovie_2->getMovieAge();
+$myMovie_2->renderMovie();
+//movie 3
+$myMovie_3->getMovieAge();
+$myMovie_3->renderMovie();
+//movie 4
+$myMovie_4->getMovieAge();
+$myMovie_4->renderMovie();
+//movie 5
+$myMovie_5->getMovieAge();
+$myMovie_5->renderMovie();
